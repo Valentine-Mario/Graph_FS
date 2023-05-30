48714911e5812ff820df11ec017e9e531566fc69
Valentine-Mario <valentine13400@gmail.com>
2023-05-30 16:06:08 +0100

    setup

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+mod schema;
+mod fs_utils;
+
+use std::{io, sync::Arc};
+
+use actix_cors::Cors;
+use actix_web::{
+    get, middleware, route,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
+use actix_web_lab::respond::Html;
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+
+use crate::schema::{create_schema, Schema};
+
+/// GraphiQL playground UI
+#[get("/graphiql")]
+async fn graphql_playground() -> impl Responder {
+    Html(graphiql_source("/graphql", None))
+}
+
+/// GraphQL endpoint
+#[route("/graphql", method = "GET", method = "POST")]
+async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+    let user = data.execute(&st, &()).await;
+    HttpResponse::Ok().json(user)
+}
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    dotenv::dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    println!("{:?}", fs_utils::get_dir());
+
+    // Create Juniper schema
+    let schema = Arc::new(create_schema());
+   
+    log::info!("starting HTTP server on port 8080");
+    log::info!("GraphiQL playground: http://localhost:8080/graphiql");
+
+    // Start HTTP server
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::from(schema.clone()))
+            .service(graphql)
+            .service(graphql_playground)
+            // the graphiql UI requires CORS to be enabled
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
+            // .wrap(ctx.clone())
+    })
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
