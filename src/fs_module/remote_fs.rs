c0ba5d9b61a2ed7e95ed0a969687cd6a8abc18b6
Valentine-Mario <valentine13400@gmail.com>
2023-06-07 09:35:01 +0100

    added create file query remote

--- src/fs_module/remote_fs.rs
+++ src/fs_module/remote_fs.rs
@@ -1,16 +1,22 @@
+use std::path::Path;
+
 use juniper::FieldResult;
 
 use crate::{
-    schema::{Context, File, Folder, Message},
+    schema::{Context, Message},
     utils::check_auth_path,
 };
 pub struct RemoteFsQuery;
 
 #[juniper::graphql_object(context = Context)]
 impl RemoteFsQuery {
-    #[graphql(description = "Returns a list of all files in directory")]
-    fn read_file_in_dir(context: &Context, path: String) -> FieldResult<Message> {
-        log::info!("authenticated {}", context.sess.authenticated());
-        Ok(Message::new(String::from("Item renamed successfully")))
+    #[graphql(description = "create file")]
+    fn create_file(context: &Context, path: String) -> FieldResult<Message> {
+        let path = Path::new(&path);
+        check_auth_path(&path)?;
+        let sftp = context.sess.sftp()?;
+        sftp.create(path)?;
+        let return_msg = format!("{:?} created successfully", path);
+        Ok(Message::new(String::from(return_msg)))
     }
 }
