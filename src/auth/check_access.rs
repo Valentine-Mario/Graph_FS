39e88b0d92a224078fd08ba961f0bf92d54335a6
Valentine-mario <valentine13400@gmail.com>
2024-02-27 16:34:29 +0100

    passed pool into middle ware

--- src/auth/check_access.rs
+++ src/auth/check_access.rs
@@ -2,11 +2,13 @@ use std::pin::Pin;
 
 use actix_web::dev::Payload;
 use actix_web::error::ErrorUnauthorized;
+use actix_web::web::{self, Data};
 use actix_web::{Error, FromRequest, HttpRequest};
 use futures::Future;
 
 use crate::auth::jwt;
 use crate::cli::Args;
+use crate::schema::GraphqlWebData;
 use crate::user_setting::manage_config::get_user;
 
 pub struct Authorized;
@@ -16,12 +18,18 @@ impl FromRequest for Authorized {
     type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
 
     fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
-        println!("called");
-        if is_authorized(req) {
-            Box::pin(async move { Ok(Authorized) })
-        } else {
-            Box::pin(async move { Err(ErrorUnauthorized("not authorized"))? })
-        }
+        let state = req
+            .app_data::<Data<GraphqlWebData>>()
+            .expect("error getting app state")
+            .to_owned();
+        let req = req.to_owned();
+        Box::pin(async move {
+            if is_authorized(req, state).await {
+                Ok(Authorized)
+            } else {
+                Err(ErrorUnauthorized("not authorized"))?
+            }
+        })
     }
 
     fn extract(req: &HttpRequest) -> Self::Future {
@@ -29,13 +37,19 @@ impl FromRequest for Authorized {
     }
 }
 
-fn is_authorized(req: &HttpRequest) -> bool {
+async fn is_authorized(req: HttpRequest, state: web::Data<GraphqlWebData>) -> bool {
     let args = Args::new();
 
     if args.use_auth.is_some() && args.use_auth.unwrap() {
         if let Some(value) = req.headers().get("authorization") {
             if let Ok(user) = jwt::decode_token(value.to_str().unwrap_or(""), args.jwt_secret) {
-                get_user(&user).is_ok()
+                state
+                    .db_conn
+                    .clone()
+                    .expect("error fetching db pool")
+                    .get_user_by_email(&user)
+                    .await
+                    .is_ok()
             } else {
                 false
             }
